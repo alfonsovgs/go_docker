c8d21940f9636473f29d80cb2c09444a3b42d6ea
Alfonso <vc.alfonso@hotmail.com>
2021-09-26 15:17:41 -0500

    docker composer

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+type Message struct {
+	value string `json:"value"`
+}
 
 func main() {
 	e := echo.New()
@@ -18,10 +23,36 @@ func main() {
 		return c.HTML(http.StatusOK, "Hello World")
 	})
 
+	e.POST("/send", func(c echo.Context) error {
+		return sendHandler(c)
+	})
+
+	e.GET("/messages", func(c echo.Context) error {
+		r, err := getMessagesHandler(c)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, r)
+	})
+
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
 		httpPort = "8080"
 	}
 
 	e.Logger.Fatal(e.Start(":" + httpPort))
+}
+
+func initStore() (*sql.DB, error) {
+	return nil, nil
+}
+
+func sendHandler(c echo.Context) error {
+	return nil
+}
+
+func getMessagesHandler(c echo.Context) (*Message, error) {
+	return nil, nil
 }
